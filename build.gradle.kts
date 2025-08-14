403923049b73cb504ad14f1719c6e15a53eeab6b
j4m1nion <mamila.xr@outlook.com>
2025-08-14 22:36:21 +0200

    update version to 1.0.5 + added LockStatus.kt deviceUnsecure

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ publishing {
                 from(components["release"])
                 groupId = "com.github.j4m1nion"
                 artifactId = "j4msec"
-                version = "1.0.4"
+                version = "1.0.5"
             }
         }
     }
